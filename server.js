411b1903874f6d65d22e256547b7430441be1fb3
Sebastian <sebastian5253@wp.pl>
2022-05-08 11:16:06 +0200

    Updates to heroku deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/inparcel-gui'));
-app.get('/*', function(req,res) {
+app.get('/', function(req,res) {
   res.sendFile(path.join(__dirname+
     '/dist/inparcel-gui/index.html'));});
 app.listen(process.env.PORT || 8080);
