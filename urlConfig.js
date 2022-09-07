729f325d5fb2b2259a29eacd262b34b4e268c0fb
Sebastian <sebastian5253@wp.pl>
2022-09-08 00:26:15 +0200

    Update application urls

--- urlConfig.js
+++ urlConfig.js
@@ -1,6 +1,9 @@
 export const globalUrl = {
   url: 'http://localhost:8080'
 }
+export const testUrl = {
+  url: 'https://inparcel-test.herokuapp.com'
+}
 export const devUrl = {
   url: 'https://inparcel-dev.herokuapp.com'
 }
